Fixed units in dispersion relation and sdf helper. Added method to characterise run parameters.

Epoch_analysis/sdf_helper_test.py
@@ -2,10 +2,12 @@ import dask.array
 import dask.array.rechunk
 from sdf_xarray import sdf, SDFPreprocess
 from pathlib import Path
-from scipy import fftpack
+from scipy import fftpack, constants
 import dask
+import epydeck
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import xarray as xr
 import argparse
 import xrft
@@ -48,10 +50,33 @@ if __name__ == "__main__":
         required = True,
         type=Path
     )
+    parser.add_argument(
+        "--fft",
+        action="store",
+        help="Quantity to fft.",
+        required = True,
+        type=str
+    )
+    parser.add_argument(
+        "--normalise",
+        action="store_true",
+        help="Normalise data around 0?",
+        required = False
+    )
+    parser.add_argument(
+        "--log",
+        action="store_true",
+        help="Log plot",
+        required = False
+    )
     args = parser.parse_args()
 
     directory = args.dir
+    quantity = args.fft
+    norm = args.normalise
+    logplot = args.log
 
+    # Read dataset
     ds = xr.open_mfdataset(
         str(directory / "*.sdf"),
         data_vars='minimal', 
@@ -60,28 +85,35 @@ if __name__ == "__main__":
         preprocess=MySdfPreprocess()
     )
 
-    data_to_plot = []
-    electric_field_ex : xr.DataArray = ds["Electric Field/Ex"]
-    electric_field_ey : xr.DataArray = ds["Electric Field/Ey"]
-    electric_field_ez : xr.DataArray = ds["Electric Field/Ez"]
-    magnetic_field_bx : xr.DataArray = ds["Magnetic Field/Bx"]
-    magnetic_field_by : xr.DataArray = ds["Magnetic Field/By"]
-    magnetic_field_bz : xr.DataArray = ds["Magnetic Field/Bz"]
-    charge_density_all : xr.DataArray = ds["Derived/Charge_Density"]
-    charge_density_irb : xr.DataArray = ds["Derived/Charge_Density/ion_ring_beam"]
-    charge_density_p : xr.DataArray = ds["Derived/Charge_Density/proton"]
-    charge_density_e : xr.DataArray = ds["Derived/Charge_Density/electron"]
+    # Read input deck
+    input = {}
+    with open(str(directory / "input.deck")) as id:
+        input = epydeck.loads(id.read())
 
-    data_to_plot.append(magnetic_field_bx)
-    data_to_plot.append(magnetic_field_by)
-    data_to_plot.append(magnetic_field_bz)
-    data_to_plot.append(electric_field_ex)
-    data_to_plot.append(electric_field_ey)
-    data_to_plot.append(electric_field_ez)
-    data_to_plot.append(charge_density_irb)
-    data_to_plot.append(charge_density_p)
-    data_to_plot.append(charge_density_e)
-    data_to_plot.append(charge_density_all)
+    data_to_plot = []
+    # electric_field_ex : xr.DataArray = ds["Electric Field/Ex"]
+    # electric_field_ey : xr.DataArray = ds["Electric Field/Ey"]
+    # electric_field_ez : xr.DataArray = ds["Electric Field/Ez"]
+    # magnetic_field_bx : xr.DataArray = ds["Magnetic Field/Bx"]
+    # magnetic_field_by : xr.DataArray = ds["Magnetic Field/By"]
+    # magnetic_field_bz : xr.DataArray = ds["Magnetic Field/Bz"]
+    # charge_density_all : xr.DataArray = ds["Derived/Charge_Density"]
+    # charge_density_irb : xr.DataArray = ds["Derived/Charge_Density/ion_ring_beam"]
+    # charge_density_p : xr.DataArray = ds["Derived/Charge_Density/proton"]
+    # charge_density_e : xr.DataArray = ds["Derived/Charge_Density/electron"]
+
+    quantity_array : xr.DataArray = ds[quantity]
+
+    # data_to_plot.append(magnetic_field_bx)
+    # data_to_plot.append(magnetic_field_by)
+    # data_to_plot.append(magnetic_field_bz)
+    # data_to_plot.append(electric_field_ex)
+    # data_to_plot.append(electric_field_ey)
+    # data_to_plot.append(electric_field_ez)
+    # data_to_plot.append(charge_density_irb)
+    # data_to_plot.append(charge_density_p)
+    # data_to_plot.append(charge_density_e)
+    # data_to_plot.append(charge_density_all)
     # data_to_plot.append(pos_charge)
     # data_to_plot.append(all_charge)
 
@@ -92,47 +124,43 @@ if __name__ == "__main__":
 
     # plt.show()
 
-    #magnetic_field_by_c = magnetic_field_by.chunk({'time': magnetic_field_by.sizes['time']})
-    print(charge_density_p.load())
-    print(charge_density_irb.load())
-    all_pos = charge_density_p.load() + charge_density_irb.load()
-    print(all_pos)
+    data_load = quantity_array.load()
 
-    data_load = charge_density_all.load()
-    #data = data - np.mean(data)
-    print(magnetic_field_bz.load())
-    Bz = np.mean(magnetic_field_bz.load(), axis=1)
     TWO_PI = 2.0 * np.pi
-    B0z = np.mean(Bz) # THINK ABOUT THIS, UNSURE IF (MOST) CORRECT
-    vTh_over_Wci = ds.coords["X_Grid_mid"] * (TWO_PI / B0z)
-    Tci = np.array(ds.coords["time"]*Bz/TWO_PI)
-    data = xr.DataArray(data_load - np.mean(data_load), coords=[Tci, vTh_over_Wci], dims=["time", "X_Grid_mid"])
+    B0 = input['constant']['b_strength']
+
+    electron_mass = input['species']['electron']['mass'] * constants.electron_mass
+    ion_bkgd_mass = input['constant']['ion_mass_e'] * constants.electron_mass 
+    ion_ring_mass = input['constant']['ion_mass_e'] * constants.electron_mass # Note: assumes background and ring beam ions are then same species
+    ion_ring_frac = input['constant']['frac_beam']
+
+    mass_density = input['constant']['background_density'] * (electron_mass + ion_bkgd_mass + (ion_ring_frac * ion_ring_mass))
+
+    ion_ring_charge = input['species']['ion_ring_beam']['charge'] * constants.elementary_charge
+    
+    ion_gyroperiod = (TWO_PI * ion_ring_mass) / (ion_ring_charge * B0)
+    Tci = ds.coords["time"] / ion_gyroperiod
+    alfven_velo = B0 / (np.sqrt(constants.mu_0 * mass_density))
+    vA_Tci = ds.coords["X_Grid_mid"] / (ion_gyroperiod * alfven_velo)
+    
+    print(np.mean(data_load))
+    data = xr.DataArray(data_load if not norm else data_load - abs(np.mean(data_load)), coords=[Tci, vA_Tci], dims=["time", "X_Grid_mid"])
     orig_spec : xr.DataArray = xrft.xrft.fft(data, true_amplitude=True, true_phase=True)
     spec = abs(orig_spec)
+    
+    # Positive times
     spec = spec.sel(freq_time=spec.freq_time>=0.0)
-    spec.plot()
+
+    # Positive wavenumbers
+    # spec = spec.sel(freq_X_Grid_mid=spec.freq_X_Grid_mid>=0.0)
+    if logplot:
+        spec.plot(norm=colors.LogNorm())
+    else:
+        spec.plot()
+    if norm:
+        plt.title(f"{quantity} (normalised) - {directory.name}")
+    else:
+        plt.title(f"{quantity} - {directory.name}")
+    plt.xlabel("Wavenumber [Wcp/Va]")
+    plt.ylabel("Frequency [Wcp]")
     plt.show()
-
-    #b0z = dataset.B0z
-    #vTh_over_Wci = np.arange(0.0, dataset.numCells) * (TWO_PI / b0z)
-    #Tci = np.array(dataset.outputTimes*b0z/TWO_PI)
-    #charge_density = xr.DataArray(dataset.chargeDensity, coords=[Tci, vTh_over_Wci], dims=["time", "space"])
-    #charge_density_irb = charge_density_irb.chunk({'time': 200})
-    #charge_density_irb = charge_density_irb.as_numpy()
-    #og_spectrum: xr.DataArray = xrft.xrft.fft(charge_density_irb, true_amplitude=False, true_phase=False)
-    #spectrum = abs(og_spectrum)
-    #spectrum.plot()
-    #plt.show()
-
-    #fwd_time_spectrum = spectrum.sel(freq_time=spectrum.freq_time>=0.0)
-    #k_vTh_over_Wci = fwd_time_spectrum.freq_space.data * ((4.0 * np.pi**2) / (b0z**2))
-    #fwd_time_spectrum = fwd_time_spectrum.assign_coords(k_perp=("freq_space", k_vTh_over_Wci))
-    #fwd_time_spectrum.plot()
-    #fwd_time_spectrum.plot(size=9, x="k_perp", y = "freq_time", cbar_kwargs={"label": "CHARGE DENSITY"})
-    #plt.title("FFT of charge density in cell space and (+ve) time", fontsize=20)
-    #plt.yticks(fontsize=24)
-    #plt.xticks(fontsize=24)
-    #plt.ylabel("$\omega/\omega_{ci}$", fontsize=32)
-    #plt.xlabel("$k/k_{\perp}v_{th,i}\omega_{ci}^{-1}$", fontsize=30)
-    #plt.grid()
-    #plt.show()