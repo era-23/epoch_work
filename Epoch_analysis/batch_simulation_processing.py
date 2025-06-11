Fixed bug in num_cells of batch metadata calculation

Epoch_analysis/batch_simulation_processing.py
@@ -35,7 +35,6 @@ def initialise_folder_structure(
     plotsFolder = outputDirectory / "plots"
                 
     if create:
-
         if debug:
             print(f"Creating folder structure in {outputDirectory}....")
 
@@ -56,7 +55,9 @@ def initialise_folder_structure(
             if plotGrowthRates:
                 gammaPlotFolder = plotFieldFolder / "growth_rates"
                 os.mkdir(gammaPlotFolder)
-
+    else:
+        if debug:
+            print(f"Using existing folder structure in {outputDirectory}....")
     
     return dataFolder, plotsFolder
 
@@ -80,22 +81,31 @@ def calculate_simulation_metadata(
     outputNcRoot.timeSamplingFreq_Hz = num_t/sim_time
     outputNcRoot.timeNyquistFreq_Hz = num_t/(2.0 *sim_time)
 
-    # Work out num cells
+    
     B0 = inputDeck['constant']['b0_strength']
     if beam:
         ion_gyrofrequency = ppf.gyrofrequency(B0 * u.T, fastSpecies)
     else:
         ion_gyrofrequency = ppf.gyrofrequency(B0 * u.T, bkgdSpecies)
     ion_gyroperiod = 1.0 / ion_gyrofrequency
-    background_density = inputDeck['constant']['background_density']
-    debye_length = ppl.Debye_length(inputDeck['constant']['background_temp'] * u.K, background_density / u.m**3)
-    electron_gyroradius = ppl.gyroradius(B = B0 * u.T, particle = "e", T=inputDeck['constant']['background_temp'] * u.K)
+    
+    outputNcRoot.ionGyrofrequency_radPs = ion_gyrofrequency.value
+    outputNcRoot.ionGyroperiod_sPrad = ion_gyroperiod.value
+    ion_gyroperiod_s = ion_gyroperiod * 2.0 * np.pi * u.rad
+    outputNcRoot.ionGyroperiod_s = ion_gyroperiod_s.value
+
+    # Work out num cells
+    background_density = inputDeck['constant']['background_density'] / u.m**3
+    background_temp = inputDeck['constant']['background_temp'] * u.K
+    debye_length = ppl.Debye_length(background_temp, background_density)
+    electron_gyroradius = np.sqrt(2.0 * constants.k * (u.J / u.K) * background_temp * constants.electron_mass * u.kg) / (constants.elementary_charge * u.C * inputDeck['constant']['b0_strength'] * u.T)
     beam_frac = inputDeck['constant']['frac_beam']
     number_density_bkgd = background_density * (1.0 - beam_frac)
-    alfven_velocity = pps.Alfven_speed(B0 * u.T, number_density_bkgd / u.m**3, bkgdSpecies)
+    alfven_velocity = pps.Alfven_speed(B0 * u.T, number_density_bkgd, bkgdSpecies)
     grid_spacing = float(np.min([debye_length.value, electron_gyroradius.value]))
     pixels_per_k = inputDeck['constant']['pixels_per_k']
-    num_cells = int(np.ceil((pixels_per_k * ion_gyroperiod.value * alfven_velocity.value) / grid_spacing))
+    est_min_cells = int(np.ceil((pixels_per_k * ion_gyroperiod_s.value * alfven_velocity.value) / grid_spacing))
+    num_cells = dataset["X_Grid_mid"].size
     outputNcRoot.numCells = num_cells
 
     sim_L = float(dataset['Magnetic_Field_Bz'].coords["X_Grid_mid"][-1]) * u.m
@@ -108,25 +118,20 @@ def calculate_simulation_metadata(
     B0_angle = inputDeck['constant']["b0_angle"]
     outputNcRoot.B0angle = B0_angle
     
-    outputNcRoot.backgroundDensity = background_density
+    outputNcRoot.backgroundDensity = background_density.value
     outputNcRoot.beamFraction = beam_frac
 
     outputNcRoot.debyeLength_m = debye_length.value
     sim_L_dl = sim_L / debye_length
     outputNcRoot.simLength_dL = sim_L_dl
     outputNcRoot.cellWidth_dL = sim_L_dl / num_cells
-
-    outputNcRoot.ionGyrofrequency_radPs = ion_gyrofrequency.value
-    outputNcRoot.ionGyroperiod_sPrad = ion_gyroperiod.value
-    ion_gyroperiod_s = ion_gyroperiod * 2.0 * np.pi * u.rad
     
-    outputNcRoot.ionGyroperiod_s = ion_gyroperiod_s.value
-    plasma_freq = ppf.plasma_frequency(number_density_bkgd / u.m**3, bkgdSpecies)
+    plasma_freq = ppf.plasma_frequency(number_density_bkgd, bkgdSpecies)
     outputNcRoot.plasmaFrequency_radPs = plasma_freq.value
 
     outputNcRoot.alfvenSpeed = alfven_velocity.value
 
-    wLH_si = ppf.lower_hybrid_frequency(B0 * u.T, number_density_bkgd / u.m**3, bkgdSpecies)
+    wLH_si = ppf.lower_hybrid_frequency(B0 * u.T, number_density_bkgd, bkgdSpecies)
     outputNcRoot.lhFrequency_radPs = wLH_si.value
     wLH_cyclo = wLH_si / ion_gyrofrequency
     outputNcRoot.lhFrequency_ionGyroF = wLH_cyclo.value
@@ -138,7 +143,7 @@ def calculate_simulation_metadata(
         outputNcRoot.fastIonGyroradius = irb_gyroradius.value
         outputNcRoot.simLength_rLfi = sim_L.value / irb_gyroradius.value
         outputNcRoot.cellWidth_rLfi = (sim_L.value / irb_gyroradius.value) / num_cells
-    proton_gyroradius = ppl.gyroradius(B = B0 * u.T, particle = "p", T=inputDeck['constant']['background_temp'] * u.K)
+    proton_gyroradius = ppl.gyroradius(B = B0 * u.T, particle = "p", T = background_temp)
     outputNcRoot.protonGyroradius = proton_gyroradius.value
     outputNcRoot.simLength_rLp = sim_L.value / proton_gyroradius.value
     outputNcRoot.cellWidth_rLp = (sim_L.value / proton_gyroradius.value) / num_cells
@@ -162,6 +167,7 @@ def calculate_simulation_metadata(
         print(f"Sim time in SI: {sim_time}")
         print(f"Sampling frequency: {num_t/sim_time}")
         print(f"Nyquist frequency: {num_t/(2.0 *sim_time)}")
+        print(f"Estimated minimum cells needed: {est_min_cells}")
         print(f"Num cells: {num_cells}")
         print(f"Sim length: {sim_L}")
         print(f"Sampling frequency: {num_cells/sim_L}")
@@ -852,6 +858,12 @@ if __name__ == "__main__":
         required = False
     )
     parser.add_argument(
+        "--clean",
+        action="store_true",
+        help="Delete existing analysis and re-write files. Otherwise will append to existing output files.",
+        required = False
+    )
+    parser.add_argument(
         "--debug",
         action="store_true",
         help="Print debugging statements.",
@@ -877,12 +889,15 @@ if __name__ == "__main__":
         outputDirectory = args.outputDir
     else:
         outputDirectory = args.dir / Path("analysis")
-        if not os.path.exists(outputDirectory):
-            os.mkdir(outputDirectory)
+        if os.path.exists(outputDirectory):
+            print(f"Existing analysis folder found at '{outputDirectory}'")
+            createFolders = False
+        else:
+            createFolders = True
+
+    dataFolder, plotsFolder = initialise_folder_structure(outputDirectory, args.fields, True if args.clean else createFolders, args.energy, args.saveGammaPlots)
 
     print(f"Using analysis folder at '{outputDirectory}'")
-
-    dataFolder, plotsFolder = initialise_folder_structure(outputDirectory, args.fields, createFolders, args.energy, args.saveGammaPlots)
 
     if args.runNumber is not None:
         args.dir = Path(os.path.join(args.dir, f"run_{args.runNumber}"))

Epoch_analysis/debugging.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from scipy import constants
 import numpy as np
 import epydeck
+import glob
 from pathlib import Path
 from plasmapy.formulary import lengths as ppl
 from plasmapy.formulary import speeds as pps
@@ -13,11 +14,11 @@ def debug_input_deck(directory : Path):
 
     # Read input deck
     input = {}
-    with open(str(directory / "input.deck")) as id:
+    with open(glob.glob(str(directory / "run_10_input.deck"))[0]) as id:
         input = epydeck.loads(id.read())
 
     input_vals = input['constant']
-    background_density = input_vals['background_density']
+    background_density = input_vals['background_density'] / u.m**3
     background_temp = input_vals['background_temp'] * u.K
     b0_strength = input_vals['b0_strength'] * u.T
 
@@ -58,7 +59,7 @@ def debug_input_deck(directory : Path):
     # Angle between B and x (predominantly in Z)
     alfven_velo = b0_strength / np.sqrt(constants.mu_0 * background_mass_density)
     #v_perp_ratio = input_vals['v_perp_ratio']
-    v_perp_ratio = 0.634
+    v_perp_ratio = 1.0
     v_perp = v_perp_ratio * alfven_velo
     #ring_beam_energy = ((v_perp_ratio * alfven_velo)**2 * mass_fast_ion) / 2.0 * constants.e
     #pring_beam = np.sqrt(2 * mass_fast_ion * constants.e * ring_beam_energy)
@@ -70,16 +71,16 @@ def debug_input_deck(directory : Path):
     E_rb_si = mass_fast_ion * (vring**2 + vbeam**2) / 2.0
     E_rb_ev = E_rb_si / constants.e
 
-    print(f"Alfven velo      = {'%.2e' % Decimal(alfven_velo)}")
-    print(f"Ring beam E (si) = {'%.2e' % Decimal(E_rb_si)}")
-    print(f"Ring beam E (ev) = {'%.2e' % Decimal(E_rb_ev)}")
-    print(f"Ring v (vperp)   = {'%.2e' % Decimal(vring)}")
-    print(f"Beam v (vpara)   = {'%.2e' % Decimal(vbeam)}")
+    print(f"Alfven velo      = {'%.2e' % Decimal(alfven_velo.value)}")
+    print(f"Ring beam E (si) = {'%.2e' % Decimal(E_rb_si.value)}")
+    print(f"Ring beam E (ev) = {'%.2e' % Decimal(E_rb_ev.value)}")
+    print(f"Ring v (vperp)   = {'%.2e' % Decimal(vring.value)}")
+    print(f"Beam v (vpara)   = {'%.2e' % Decimal(vbeam.value)}")
     print(f"Intended v_perp/v_A = {'%.2e' % Decimal(v_perp_ratio)}")
-    print(f"Actual v_perp/v_A   = {'%.2e' % Decimal(vring / alfven_velo)}")
+    print(f"Actual v_perp/v_A   = {'%.2e' % Decimal(vring.value / alfven_velo.value)}")
 
-    my_Debye_length = np.sqrt(constants.epsilon_0 * constants.k * background_temp / background_density / constants.elementary_charge**2) * u.m
-    pp_Debye_length = ppl.Debye_length(background_temp, background_density / u.m**3)
+    my_Debye_length = np.sqrt(constants.epsilon_0 * constants.k * background_temp.value / background_density.value / constants.elementary_charge**2) * u.m
+    pp_Debye_length = ppl.Debye_length(background_temp, background_density)
     print(f"My Debye length: {my_Debye_length}")
     print(f"PP Debye length: {pp_Debye_length}")
 
@@ -90,22 +91,23 @@ def debug_input_deck(directory : Path):
     else:
         print("Electron gyroradius is smaller, using this for cell width.")
         cell_width = combined_rLe
-    print(f"Cell width: {cell_width}")
+    print(f"Cell width: {cell_width} ({cell_width/my_Debye_length} Debye lengths) ({cell_width/combined_rLe} e- Larmor radii)")
     
-    num_cells = input_vals["num_cells"]
+    ion_gyroperiod_s = (2.0 * np.pi * u.rad) / ppf.gyrofrequency(b0_strength * u.T, "p+")
+    print(f"Ion gyroperiod = {1.0 / ppf.gyrofrequency(b0_strength * u.T, 'p+')} or {(2.0 * np.pi * u.rad) / ppf.gyrofrequency(b0_strength * u.T, 'p+')}")
+    needed_ppk = 8.0
+    needed_num_cells = np.ceil((needed_ppk * ion_gyroperiod_s * alfven_velo) / cell_width).astype(int)
+    print(f"Minimum number of cells for ppk = {needed_ppk} is {needed_num_cells}")
+    num_cells = needed_num_cells
+    print(f"Num Cells : {num_cells}")
     sim_L = num_cells * cell_width * u.m
     print(f"Sim length : {sim_L}")
-    ion_gyroperiod_s = (2.0 * np.pi * u.rad) / ppf.gyrofrequency(b0_strength * u.T, "p+")
     sim_L_vA_Tci = sim_L / (ion_gyroperiod_s * alfven_velo * (u.m / u.s))
     print(f"Sim length in k units: {sim_L_vA_Tci}")
     spatial_Nyquist = num_cells/(2.0 * sim_L_vA_Tci)
     print(f"Spatial Nyquist frequency: {spatial_Nyquist}")
     pixels_per_wavenumber = num_cells / (2.0 * spatial_Nyquist)
     print(f"Pixels per wavenumber: {pixels_per_wavenumber}")
-
-    needed_ppk = 8.0
-    needed_num_cells = np.ceil((needed_ppk * ion_gyroperiod_s * alfven_velo) / cell_width).astype(int)
-    print(f"Minimum number of cells for ppk = {needed_ppk} is {needed_num_cells}")
 
     # all_variables = dir() 
   

Epoch_analysis/energy_analysis.py
@@ -2,6 +2,7 @@ import glob
 from sdf_xarray import SDFPreprocess
 from pathlib import Path
 from scipy import constants
+from inference.plotting import matrix_plot
 import pandas as pd
 import matplotlib.pyplot as plt
 import epydeck
@@ -334,6 +335,70 @@ def correlate_cellWidth_energy_transer(analysisDirectory : Path, logColourbar : 
     # ax2.scatter(B0_str, rLe_cellWidth_ratio, c=hasFiTroughBiPeak, cmap="Greens")
     # ax3.scatter(density, rLe_cellWidth_ratio, c=bkgdIonChangeAtFastIonTrough_pct, cmap="Blues")
 
+def analyse_peak_characteristics(analysisDirectory : Path):
+    dataFiles = glob.glob(str(analysisDirectory / "*.nc"))
+
+    B0strength = []
+    B0angle = []
+    densities = []
+    beamFractions = []
+    hasFastIonTrough = []
+    hasBkgdIonPeak = []
+
+    for simulation in dataFiles:
+
+        data = xr.open_datatree(
+            simulation,
+            engine="netcdf4"
+        )
+
+        energyStats = data["Energy"]
+
+        if (energyStats["fastIonMeanEnergyDensity"].attrs["hasTroughs"] and energyStats["protonMeanEnergyDensity"].attrs["hasPeaks"]):
+
+            
+            mciB0.append(data.attrs["B0strength"])
+            mciBangle.append(data.attrs["B0angle"])
+            mciBeamFracs.append(data.attrs["beamFraction"])
+            mciDensities.append(data.attrs["backgroundDensity"])
+        else:
+            noMciB0.append(data.attrs["B0strength"])
+            noMciBangle.append(data.attrs["B0angle"])
+            noMciBeamFracs.append(data.attrs["beamFraction"])
+            noMciDensities.append(data.attrs["backgroundDensity"])
+    
+    mciName = "E transfer"
+    noMciName = "No E transfer"
+    mciXAxis = [mciName for _ in mciB0]
+    noMciXAxis = [noMciName for _ in noMciB0]
+
+    fig, axs = plt.subplots(1, 4, sharey=False, figsize=[12,8])
+    axs[0].scatter(mciXAxis, mciB0)
+    axs[0].scatter(noMciXAxis, noMciB0)
+    #axs[0].scatter([mciName], [np.mean(mciB0)], "x", color = "purple")
+    #axs[0].scatter([noMciName], [np.mean(noMciB0)], "x", color = "red")
+    axs[0].set_ylabel("B0 [T]")
+    axs[1].scatter(mciXAxis, mciBangle)
+    axs[1].scatter(noMciXAxis, noMciBangle)
+    #axs[1].scatter([mciName], [np.mean(mciBangle)], "x", color = "purple")
+    #axs[1].scatter([noMciName], [np.mean(noMciBangle)], "x", color = "red")
+    axs[1].set_ylabel("B0 angle [degrees]")
+    axs[2].scatter(mciXAxis, mciDensities)
+    axs[2].scatter(noMciXAxis, noMciDensities)
+    #axs[2].scatter([mciName], [np.mean(mciDensities)], "x", color = "purple")
+    #axs[2].scatter([noMciName], [np.mean(noMciDensities)], "x", color = "red")
+    axs[2].set_ylabel("Density [/m^3]")
+    axs[2].set_yscale("log")
+    axs[3].scatter(mciXAxis, mciBeamFracs)
+    axs[3].scatter(noMciXAxis, noMciBeamFracs)
+    #axs[3].scatter([mciName], [np.mean(mciBeamFracs)], "x", color = "purple")
+    #axs[3].scatter([noMciName], [np.mean(noMciBeamFracs)], "x", color = "red")
+    axs[3].set_yscale("log")
+    axs[3].set_ylabel("Beam fraction [au]")
+    
+    plt.tight_layout()
+    plt.show()
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("parser")
@@ -384,9 +449,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.electronHeating:
-        analyse_electron_heating(args.dir)
-    elif args.correlate:
-        correlate_cellWidth_energy_transer(args.dir)
-    else:
-        calculate_energy(args.dir, args.irb, args.pct)
+    analyse_peak_characteristics(Path("/home/era536/Documents/Epoch/Data/irb_may25_analysis/data/"))
+
+    # if args.electronHeating:
+    #     analyse_electron_heating(args.dir)
+    # elif args.correlate:
+    #     correlate_cellWidth_energy_transer(args.dir)
+    # else:
+    #     calculate_energy(args.dir, args.irb, args.pct)