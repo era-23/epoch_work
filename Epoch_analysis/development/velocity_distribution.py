821b22bf0c9a37e02ba3c20d990c3cadd033b21d
era-23 <115151590+era-23@users.noreply.github.com>
2025-09-30 15:40:40 +0100

    Added velocity distribution recovery. Made velo plots apply to one sdf file (timepoint) only.

--- Epoch_analysis/development/velocity_distribution.py
+++ Epoch_analysis/development/velocity_distribution.py
@@ -7,6 +7,7 @@ from decimal import Decimal
 from plasmapy.formulary import speeds as pps
 from plasmapy.formulary import frequencies as ppf
 from scipy.stats import linregress
+from scipy.optimize import curve_fit
 import astropy.units as u
 import xarray as xr
 import numpy as np
@@ -16,12 +17,54 @@ import argparse
 def gaussian(x, a, mean, sigma):
     return a * np.exp(-((x - mean)**2 / (2 * sigma**2)))
 
-def plot_velo_dists(directory : Path, files : list, vPerp : bool = False, saveFolder : Path = None, doLog : bool = True):
+def eval_pRingBeam(fi_mass : float, fi_charge : float, ring_beam_energy : float):
+    return np.sqrt(2.0 * fi_mass * fi_charge * ring_beam_energy)
 
-    startTime_s = 0.0
-    speciess = ["electron", "deuteron", "alpha"]
+def eval_pBeam(p_ring_beam : float, pitch : float):
+    return p_ring_beam * pitch
+
+def eval_pRing(p_ring_beam : float, pitch : float):
+    return p_ring_beam * np.sqrt(1.0 - pitch**2)
+
+def eval_pFastSpread(p_ring : float):
+    return 0.06 * p_ring
+
+def eval_px_peak(p_ring : float, p_fast_spread : float):
+    return (p_ring / 2.0) * (1.0 + np.sqrt(1.0 + (2.0 * (p_fast_spread/p_ring))**2))
+
+def dist_func_orig(V, px_peak : float, p_ring : float, p_fast_spread : float, p_beam : float):
+    vx, vz = V
+    px = vx * 7294.3 * constants.electron_mass
+    pz = vz * 7294.3 * constants.electron_mass
+    dist_fn = []
+    assert len(px) == len(pz)
+    for i in range(len(px)):
+        dist_fn.append((px[i] / px_peak) * np.exp(-0.5 * ((px[i] - p_ring) / p_fast_spread)**2) * np.exp(-0.5 * ((pz[i] - p_beam) / p_fast_spread)**2))
+    return np.array(dist_fn)
+
+# def dist_func_forFitting(V, px_peak : float, p_ring : float, p_fast_spread : float, p_beam : float):
+#     vx, vz = V
+#     px = vx * 7294.3 * constants.electron_mass
+#     pz = vz * 7294.3 * constants.electron_mass
+#     dist_fn = []
+#     assert len(px) == len(pz)
+#     for i in range(len(px)):
+#         dist_fn.append((px[i] / px_peak) * np.exp(-0.5 * (((px[i] - p_ring)**2 + (pz[i] - p_beam)**2) / p_fast_spread**2)))
+#     return np.array(dist_fn)
+
+def dist_func_zOnly(vz, px_over_px_peak : float, p_fast_spread : float, p_beam : float):
+    pz = vz * 7294.3 * constants.electron_mass
+    return px_over_px_peak * np.exp(-0.5 * ((pz - p_beam) / p_fast_spread)**2)
+
+def dist_func_xOnly(vx, px_peak : float, p_fast_spread : float, p_ring : float):
+    px = vx * 7294.3 * constants.electron_mass
+    return (px / px_peak) * np.exp(-0.5 * ((px - p_ring) / p_fast_spread)**2)
+
+def plot_velo_dists(directory : Path, file : str, vPerp : bool = False, saveFolder : Path = None, doLog : bool = True):
+
+    # speciess = ["electron", "deuteron", "alpha"]
+    speciess = ["alpha"]
     dims = ["Vx", "Vy", "Vz"]
-    colors = ["red", "orange", "green", "blue", "black"]
 
     # Read input deck
     inputDeck = {}
@@ -32,101 +75,140 @@ def plot_velo_dists(directory : Path, files : list, vPerp : bool = False, saveFo
     for species in speciess:
         vTh = pps.thermal_speed(temp, "e-" if species == "electron" else "p+")
         velocities = {dim : {} for dim in dims}
-        for file in files:
-            # Read dataset
-            ds = xr.open_dataset(
-                str(directory / f"{file}.sdf"),
-                keep_particles = True
-            )
-            
-            if file == "0000":
-                startTime_s = float(ds.time)       
-            
-            time_s = float(ds.time) - startTime_s
 
-            # Read input deck
-            input = {}
-            with open(str(directory / "input.deck")) as id:
-                input = epydeck.loads(id.read())
-            
-            B0 = input['constant']['b0_strength']
-            # alfven_velo = pps.Alfven_speed(B = B0 * u.T, density = input['constant']['background_density'] / u.m**3, ion = "p")
-            fi_gyroperiod = (2.0 * np.pi * u.rad) / ppf.gyrofrequency(B = B0 * u.T, particle = "p")
-            time_tci = time_s * u.s / fi_gyroperiod
+        # Read dataset
+        ds = xr.open_dataset(
+            str(directory / f"{file}.sdf"),
+            keep_particles = True
+        )
 
-            for dim in velocities.keys(): # Runs out of memory here with many particles on my laptop
-                data = ds[f"Particles_{dim}_{species}"].load().data
-                #print(f"Max  velo: {data.max()}")
-                #print(f"Min  velo: {data.min()}")
-                #print(f"Mean velo: {np.mean(data)}")
-                #print(f"S.D. velo: {np.std(data)}")
-                velocities[dim][time_tci] = data
-                del(data)
-        
-        for dimension in velocities.keys():
+        # Read input deck
+        input = {}
+        with open(str(directory / "input.deck")) as id:
+            input = epydeck.loads(id.read())
+
+        for dim in velocities.keys(): # Runs out of memory here with many particles on my laptop
+            data = ds[f"Particles_{dim}_{species}"].load().data
+            #print(f"Max  velo: {data.max()}")
+            #print(f"Min  velo: {data.min()}")
+            #print(f"Mean velo: {np.mean(data)}")
+            #print(f"S.D. velo: {np.std(data)}")
+            velocities[dim] = data
+            del(data)
+
+        if "Vx" in velocities and "Vy" in velocities:
+            velocities["Vperp"] = np.sqrt(np.array(velocities["Vx"])**2 + np.array(velocities["Vy"])**2)
+
+        if species == "alpha":
+            p_ring_beam = eval_pRingBeam(input['constant']['fast_ion_mass_e'] * constants.electron_mass, input['constant']['fast_ion_charge_e'] * constants.elementary_charge, float(input['constant']['ring_beam_energy']))
+            print(f"p_ring_beam = {p_ring_beam}")
+            p_beam = eval_pBeam(p_ring_beam, input['constant']['pitch'])
+            print(f"p_beam = {p_beam}")
+            p_ring = eval_pRing(p_ring_beam, input['constant']['pitch'])
+            print(f"p_ring = {p_ring}")
+            p_fast_spread = eval_pFastSpread(p_ring)
+            print(f"p_fast_spread = {p_fast_spread}")
+            px_peak = eval_px_peak(p_ring, p_fast_spread)
+            print(f"px_peak = {px_peak}")
+
+        dimHists = dict.fromkeys(velocities.keys())
+
+        dims = ["Vx", "Vy", "Vperp", "Vz"]
+        for dimension in dims:
             print(f"Processing {species}: {dimension}....")
-            i = 0
-            doPlot = False
             # For bin widths/range:
             dimData = velocities[dimension]
-            upper_lim = max(vels.max() for vels in dimData.values())
-            lower_lim = min(vels.min() for vels in dimData.values())
+            upper_lim = dimData.max()
+            lower_lim = dimData.min()
             fig, ax = plt.subplots(figsize=(10,8))
-            for timepoint in dimData.keys():
-                data = dimData[timepoint]
-                print(f"timepoint: {timepoint:.3f}, total particles: {len(data)}")
-                bin_num = round(np.sqrt(len(data)))
-                # print(f"Default bin count: {bin_num}")
-                bin_num = int(np.min([1000, bin_num]))
-                # print(f"Using {bin_num} bins")
-                mu = r'$\mu$'
-                sigma = r'$\sigma$'
-                vTh_unit = r'$v_{Th}$'
-                vTh2_unit = r'$v_{Th}^2$'
-                mean_vTh = (np.mean(data)*u.m/u.s) / vTh
-                sigma_vTh2 = (np.var(data)*u.m**2/u.s**2) / vTh**2
-                stats = f"{mu}: {mean_vTh:.3e}{vTh_unit}, {sigma}: {sigma_vTh2:.3e}{vTh2_unit}, Skew: {skew(data):.3e} (p-value: {skewtest(data).pvalue:.3f}),  Kurtosis: {kurtosis(data):.3e} (p-value: {kurtosistest(data).pvalue:.3f})"
-                #print(f"{species}, {dimension}, t={timepoint:.2f} -- {stats}")
-                if i == 0:
-                    _, bins, _ = ax.hist(
-                        data, 
-                        bins = bin_num, 
-                        range=[lower_lim, upper_lim], 
-                        log=doLog, 
-                        histtype='bar', 
-                        edgecolor = colors[i], 
-                        alpha = 1.0, 
-                        ls = "solid", 
-                        label = f"t = {timepoint:.2f} -- {stats}"
-                    )
-                else:
-                    ax.hist(
-                        data, 
-                        bins = bins, 
-                        log=doLog, 
-                        histtype='bar', 
-                        edgecolor = colors[i], 
-                        alpha = 1.0, 
-                        ls = "solid", 
-                        label = f"t = {timepoint:.2f} -- {stats}"
-                    )
-                i += 1
-                doPlot = True
 
-            if doPlot:
-                # Shrink current axis by 20%
-                box = ax.get_position()
-                ax.set_position([box.x0, box.y0 + (0.2 * box.height), box.width, (box.height*0.8)])
-                ax.legend(bbox_to_anchor=(0.5, -0.1), loc = "upper center")
-                ax.set_xlabel("Velocity [m/s]")
-                ax.set_ylabel("Particle count")
-                ax.set_title(f"{species}: {dimension}")
-                if saveFolder is not None:
-                    filepath = saveFolder / f"{species}_{dimension}_log.png" if doLog else saveFolder / f"{species}_{dimension}.png"  
-                    fig.savefig(filepath, bbox_inches = "tight")
-                else:
-                    plt.show()
-                plt.close()
+            print(f"total particles: {len(dimData)}")
+            bin_num = round(np.sqrt(len(dimData)))
+            bin_num = int(np.min([1000, bin_num]))
+            print(f"Using {bin_num} bins")
+            mu = r'$\mu$'
+            sigma = r'$\sigma$'
+            vTh_unit = r'$v_{Th}$'
+            vTh2_unit = r'$v_{Th}^2$'
+            mean_vTh = (np.mean(dimData)*u.m/u.s) / vTh
+            sigma_vTh2 = (np.var(dimData)*u.m**2/u.s**2) / vTh**2
+            stats = f"{mu}: {mean_vTh:.3e}{vTh_unit}, {sigma}: {sigma_vTh2:.3e}{vTh2_unit}, Skew: {skew(dimData):.3e} (p-value: {skewtest(dimData).pvalue:.3f}),  Kurtosis: {kurtosis(dimData):.3e} (p-value: {kurtosistest(dimData).pvalue:.3f})"
+            
+            hist = ax.hist(
+                dimData, 
+                bins = bin_num, 
+                density = True,
+                range=[lower_lim, upper_lim], 
+                log=doLog, 
+                histtype='bar', 
+                alpha = 1.0, 
+                ls = "solid", 
+                label = stats
+            )
+
+            dimHists[dimension] = hist
+
+            box = ax.get_position()
+            if species == "alpha":
+                # skip_n = 100
+                # Vx = velocities["Vperp"][0::skip_n]
+                # Vz = np.array(velocities["Vz"][0::skip_n])
+                # dist_fn = dist_func_orig((Vx, Vz), px_peak, p_ring, p_fast_spread, p_beam)
+                # dist_fn_2 = dist_func_forFitting((Vx, Vz), px_peak, p_ring, p_fast_spread, p_beam)
+                # assert (np.allclose(dist_fn,dist_fn_2))
+
+                # if dimension == "Vperp":
+                #     ax2 = ax.twinx()
+                #     # Fit dist_func
+                #     Vx_bins = dimHists["Vperp"][1][:-1]
+                #     Vx_vals = dimHists["Vperp"][0]
+                #     parameters_xOnly, pcovX = curve_fit(dist_func_xOnly, Vx_bins, Vx_vals, p0=(px_peak, p_fast_spread, p_ring))
+                #     print(f"Original px_peak:       {px_peak}, Fit px_peak:       {parameters_xOnly[0]} ({(parameters_xOnly[0]*100.0/px_peak):.4f}%)")
+                #     print(f"Original p_fast_spread: {p_fast_spread},  Fit p_fast_spread: {parameters_xOnly[1]} ({(parameters_xOnly[1]*100.0/p_fast_spread):.4f}%)")
+                #     print(f"Original p_ring:        {p_ring},  Fit p_ring:        {parameters_xOnly[2]} ({(parameters_xOnly[2]*100.0/p_ring):.4f}%)")
+                #     print(f"Errors: {np.sqrt(np.diag(pcovX))}")
+                #     ax2.plot(Vx_bins, dist_func_xOnly(Vx_bins, *parameters_xOnly), color="g", label = "Dist func in Vperp only")
+                #     ax2.set_ylabel('dist_func')
+                #     ax2.set_ylim(bottom=0.0)
+
+                if dimension == "Vz":
+                    ax2 = ax.twinx()
+                    # Fit dist_func
+                    # parameters, pcov = curve_fit(dist_func_zOnly, bins[:-1], particle_count, p0=(2e6, 9e2, 1e6, 9e2))
+                    Vx_bins = dimHists["Vperp"][1][:-1]
+                    Vz_bins = dimHists["Vz"][1][:-1]
+                    Vz_vals = dimHists["Vz"][0]
+
+                    parameters, pcov = curve_fit(dist_func_orig, (Vx_bins, Vz_bins), Vz_vals, p0=(px_peak, p_ring, p_fast_spread, p_beam))
+                    print(f"Original px_peak:       {px_peak}, Fit px_peak:       {parameters[0]} ({(parameters[0]*100.0/px_peak):.4f}%)")
+                    print(f"Original p_ring:        {p_ring},  Fit p_ring:        {parameters[1]} ({(parameters[1]*100.0/p_ring):.4f}%)")
+                    print(f"Original p_fast_spread: {p_fast_spread},  Fit p_fast_spread: {parameters[2]} ({(parameters[2]*100.0/p_fast_spread):.4f}%)")
+                    print(f"Original p_beam:        {p_beam}, Fit p_beam:        {parameters[3]}  ({(parameters[3]*100.0/p_beam):.4f}%)")
+                    print(f"Errors: {np.sqrt(np.diag(pcov))}")
+                    ax2.plot(Vz_bins, dist_func_orig((Vx_bins, Vz_bins), *parameters), color="r", label = "Fit dist func")
+
+                    # parameters_zOnly, pcovZ = curve_fit(dist_func_zOnly, Vz_bins, Vz_vals, p0=(px_peak, p_fast_spread, p_beam))
+                    # print(f"Original px_peak:       {px_peak}, Vz Fit px_over_px_peak:       {parameters[0]} ({(parameters[0]*100.0/px_peak):.4f}%)")
+                    # print(f"Original p_fast_spread: {p_fast_spread},  Vz Fit p_fast_spread: {parameters[1]} ({(parameters[1]*100.0/p_fast_spread):.4f}%)")
+                    # print(f"Original p_beam:        {p_beam}, Vz Fit p_beam:        {parameters[2]}  ({(parameters[1]*100.0/p_beam):.4f}%)")
+                    # print(f"Errors: {np.sqrt(np.diag(pcovZ))}")
+                    # ax2.plot(Vz_bins, dist_func_zOnly(Vz_bins, *parameters_zOnly), color="g", label = "Dist func in Vz only")
+                    ax2.set_ylabel('dist_func')
+                    ax2.set_ylim(bottom=0.0)
+
+                fig.tight_layout()
+            
+            ax.set_position([box.x0, box.y0 + (0.2 * box.height), box.width, (box.height*0.8)])
+            ax.legend(bbox_to_anchor=(0.5, -0.1), loc = "upper center")
+            ax.set_xlabel("Velocity [m/s]")
+            ax.set_ylabel("Particle probability density")
+            ax.set_title(f"{species}: {dimension}")
+            if saveFolder is not None:
+                filepath = saveFolder / f"{species}_{dimension}_log.png" if doLog else saveFolder / f"{species}_{dimension}.png"  
+                fig.savefig(filepath, bbox_inches = "tight")
+            else:
+                plt.show()
+            plt.close()
 
 def plot_velo_3d(directory : Path, files : list, saveFolder : Path = None):
     
@@ -214,7 +296,6 @@ def plot_velo_3d(directory : Path, files : list, saveFolder : Path = None):
         #     upper_lim = max(vels.max() for vels in dimData.values())
         #     lower_lim = min(vels.min() for vels in dimData.values())
         
-
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser("parser")
@@ -226,12 +307,11 @@ if __name__ == "__main__":
         type=Path
     )
     parser.add_argument(
-        "--files",
+        "--file",
         action="store",
-        help="Indices of SDF files to process.",
+        help="Index of SDF file to process.",
         required = True,
-        type=str,
-        nargs="*"
+        type=str
     )
     parser.add_argument(
         "--vperp",
@@ -263,4 +343,4 @@ if __name__ == "__main__":
     if args.ddd:
         plot_velo_3d(args.dir, args.files, args.saveFolder)
     else:
-        plot_velo_dists(args.dir, args.files, args.vperp, args.saveFolder, args.log)
+        plot_velo_dists(args.dir, args.file, args.vperp, args.saveFolder, args.log)
