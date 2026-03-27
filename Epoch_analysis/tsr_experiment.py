Fixed unit conversion issue

Epoch_analysis/tsr_experiment.py
@@ -702,7 +702,7 @@ def regress_scanFrequencies(
     for frequency_bandwidth in frequencyBandwidths:
         
         print(f"Spectra range from a maximum frequency of {(np.min(last_freqs) * u.Hz).to(u.MHz)} to {(np.max(last_freqs) * u.Hz).to(u.MHz)}. Truncating to {frequency_bandwidth} {frequencyUnit}")
-        frequency_bandwidth = float((frequency_bandwidth * u.MHz).to(u.Hz)) if frequencyUnit == 'MHz' else frequency_bandwidth
+        frequency_bandwidth = float((frequency_bandwidth * u.MHz).to(u.Hz).value) if frequencyUnit == 'MHz' else frequency_bandwidth
         inputData = []
         for field in inputSpectraNames:
             specs = copy.deepcopy(inputs[field])